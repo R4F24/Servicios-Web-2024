6bf9266961c0cc939b1ad33095253f961ac20ef2
Erick Limon <118641200+ErickLimonG@users.noreply.github.com>
2024-04-10 08:52:19 -0600

    Updated backend
    Now includes working dockerfile with working page backend and frontend

--- backend/DataModeling.py
+++ backend/DataModeling.py
@@ -1,15 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cqlalchemy import CQLAlchemy
 from flask_cors import CORS
-
-from flask import jsonify
 import time
 import uuid
 import datetime
-
 from kasa import SmartPlug
 import subprocess
 import json
+import threading
 
 app = Flask(__name__)
 CORS(app)
@@ -17,9 +15,7 @@ app.config['CASSANDRA_HOSTS'] = ['127.0.0.1']
 app.config['CASSANDRA_KEYSPACE'] = "energy_meter"
 db = CQLAlchemy(app)
 
-#dev = SmartPlug("192.168.1.74")
-#usage = dev.modules["emeter"]
-
+#Definition of the database model
 class emeterdata(db.Model):
     id = db.columns.UUID(primary_key=True, default=uuid.uuid4)
     time_inserted = db.columns.DateTime()
@@ -29,29 +25,23 @@ class emeterdata(db.Model):
     voltage_mv = db.columns.Integer()
     total_wh = db.columns.Integer()
 
+#Hello!
 @app.route('/')
 def index():
     return 'Hello!'
 
+#Returns json of energy metrics in this instant
 @app.route('/energy')
 def get_energy():
-    # Se hace porque la API no provisiona datos JSON
-    # Mientras que la interfaz de CLI si por alguna razon
-
-    # Correr el comando y capturar el output
     output = subprocess.check_output(['kasa', '--host', '192.168.1.64', '--json', 'emeter'])
-
-    # Decodificar el byte string a un string regular
     output_str = output.decode('utf-8')
     json_energy = json.loads(output_str)
     return json_energy
 
+#Returns ALL registries from the DB
 @app.route('/getdt')
 def get_data():
-    # Realizar la consulta utilizando el método query
     results = emeterdata.filter(emeterdata.err_code == False).allow_filtering().all()
-
-    # Procesar los resultados
     data = []
     for result in results:
         data.append({
@@ -59,16 +49,26 @@ def get_data():
             'current_ma': result.current_ma,
             'err_code': result.err_code,
             'power_mw': result.power_mw,
-            'time_inserted': str(result.time_inserted),  # Convertir a cadena para JSON
+            'time_inserted': str(result.time_inserted),
             'total_wh': result.total_wh,
             'voltage_mv': result.voltage_mv
         })
-
-    # Devolver los datos como JSON
     return jsonify(data)
 
+#Inserta los datos cada 5 segundos
+def insert_data_periodically():
+    while True:
+        currentDatetime = datetime.datetime.now()
+        table = emeterdata.create(current_ma=int(get_energy()['current_ma']),
+                                  power_mw=int(get_energy()['power_mw']),
+                                  voltage_mv=int(get_energy()['voltage_mv']),
+                                  total_wh=int(get_energy()['total_wh']),
+                                  err_code=int(get_energy()['err_code']),
+                                  time_inserted=currentDatetime)
+        print("hola")
+        time.sleep(5)  # Espera 5 segundos antes de la próxima inserción
+
 db.sync_db()
-currentDatetime = datetime.datetime.now()
-user1 = emeterdata.create(current_ma=int(get_energy()['current_ma']),power_mw=int(get_energy()['power_mw'])
-                          ,voltage_mv = int(get_energy()['voltage_mv']),total_wh=int(get_energy()['total_wh']),
-                          err_code=int(get_energy()['err_code']),time_inserted=currentDatetime)
+print("hola2")
+threading.Thread(target=insert_data_periodically).start()  # Inicia el hilo para la inserción periódica
+#app.run(debug=True)  # Ejecuta la aplicación Flask
