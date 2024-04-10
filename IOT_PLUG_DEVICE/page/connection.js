baa49b2186659cd6a17a0071385102f7115de24b
Erick Limon <118641200+ErickLimonG@users.noreply.github.com>
2024-04-10 08:55:54 -0600

    Updated back-front folder name
    Updated name of the folder to better reflect what it is

--- IOT_PLUG_DEVICE/page/connection.js
+++
@@ -1,5 +0,0 @@
-let backendIp = "http://127.0.0.1:5000/energy"
-
-async function getEnergyData(){
-    fetch(backendIp).then((response) =>{return response.json().then((data) =>{console.log(data)})});
-}
