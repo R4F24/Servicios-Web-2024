3f49717fbde8f27c1bf1bccadb07b003248d91ec
Rafael De La Torre Arias <132729154+R4F24@users.noreply.github.com>
2024-04-26 15:47:31 -0600

    Delete dockers directory

--- dockers/DB_Dockers/Docker-db-hist/cassandra-sec.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-grep -rl "authenticator: AllowAllAuthenticator" /etc/cassandra | xargs sed -i 's/authenticator: AllowAllAuthenticator/authenticator: PasswordAuthenticator/g'
-
-grep -rl "authorizer: AllowAllAuthorizer" /etc/cassandra | xargs sed -i 's/authorizer: AllowAllAuthorizer/authorizer: org.apache.cassandra.auth.CassandraAuthorizer/g'
-
-
